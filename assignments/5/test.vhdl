7b1eb2e105b8bbb84357ee415de9d47da06e25c7
Nikola Babic <nikola.babic@student.etf.unibl.org>
2023-10-26 20:17:19 +0200

    Issue #5 : - Updated code

---
+++ assignments/5/test.vhdl
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test is port(
+    data_in, clk : in std_logic;
+    data_out : out std_logic;
+);
+end test;
+
+architecture test_beh of test is
+begin
+
+end test_beh;
