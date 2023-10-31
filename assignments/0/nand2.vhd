11b61d779050e2ad2fda1bc35ed2508228f81d02
Mladen Knezic <mladen.knezic@etf.unibl.org>
2023-10-31 22:50:52 +0100

    Issue #0 : Added initial design

---
+++ assignments/0/nand2.vhd
@@ -0,0 +1,10 @@
+entity nand2 is
+   port ( A_i, B_i: in std_logic;
+          Y_o: out std_logic
+        );
+end nand2;
+
+architecture beh_nand2 of nand2 is
+begin
+   Y_o <= A_i nand B_i;
+end beh_nand2;
