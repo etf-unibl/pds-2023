d4af639dfc60e773960dd0bf545c79ba018923f8
Dajana Legenovic <dajana.legenovic@student.etf.unibl.org>
2023-11-05 13:02:21 +0100

    Issue #27 : Create simple ALU circuit

--- assignments/27/simple_alu.vhd
+++ assignments/27/simple_alu.vhd
@@ -11,28 +11,16 @@ entity simple_alu is
 end simple_alu;
 
 architecture beh_arch of simple_alu is
-signal r : std_logic_vector(3 downto 0);
 signal tmp : std_logic_vector(4 downto 0);
 
 begin
-	process(i_A, i_B, i_SEL)
-begin
-		case i_SEL is 
-		when "00" => 
-			r <= std_logic_vector(unsigned(i_A)+unsigned(i_B));
-		when "01" => 
-			r <= std_logic_vector(unsigned(i_A)-unsigned(i_B));
-		when "10" => 
-			r <= "00" & i_A(3 downto 2);
-		when others =>
-			r <= (others=>'0'); 
-		
-		end case; 
-		
-		o_RES <= r; 
+		o_RES <= std_logic_vector(unsigned(i_A) + unsigned(i_B)) when (i_SEL="00") else
+					std_logic_vector(unsigned(i_A) - unsigned(i_B)) when (i_SEL="01") else
+					"00" & i_A(3 downto 2) when (i_SEL="10") else
+					(others=>'0');
+		 
 		tmp <= std_logic_vector(unsigned('0' & i_A) + unsigned('0' & i_B));
 		o_C <=tmp(4);
 		
-	end process;
 end beh_arch;
 
