f69e8454e27c14be79af1bbcf622449785541562
Dajana Legenovic <dajana.legenovic@student.etf.unibl.org>
2023-11-07 16:28:33 +0100

    Issue #27 : Create simple ALU circuit

--- assignments/27/simple_alu.vhd
+++ assignments/27/simple_alu.vhd
@@ -15,9 +15,9 @@ signal sum, diff, sarA, shlB : std_logic_vector(3 downto 0);
 signal carry : std_logic; 
 
 begin
-		sum <= std_logic_vector(unsigned(i_A) + unsigned(i_B)) when (i_SEL="00");
-		diff <= std_logic_vector(unsigned(i_A) - unsigned(i_B)) when (i_SEL="01");
-		sarA <= "00" & i_A(3 downto 2);
+		sum <= std_logic_vector(unsigned(i_A) + unsigned(i_B));
+		diff <= std_logic_vector(unsigned(i_A) - unsigned(i_B));
+		sarA <= "00"&i_A(3 downto 2);
 		shlB <= (others=>'0');
 		
 		o_RES <= sum when i_SEL(1 downto 0) ="00" else
@@ -25,9 +25,10 @@ begin
 					sarA when i_SEL(1 downto 0) ="10" else
 					shlB;
 					
-		carry <= '1' when (i_SEL="00") and (unsigned(i_A) + unsigned(i_B) > 15) else
-					'1' when (i_SEL="01") and (unsigned(i_A) < unsigned(i_B)) else 
+		carry <= '1' when (i_SEL(1 downto 0) ="00") and (unsigned(i_A) + unsigned(i_B) > 15) else
+					'1' when (i_SEL(1 downto 0) ="01") and (unsigned(i_A) < unsigned(i_B)) else 
 					'0';
+					
 		o_C <= carry; 
 		
 		
