bd331e7a5569d0ef6cb2caa5d77f0be66da386b9
Nikola Babic <nikola.babic@student.etf.unibl.org>
2023-11-28 17:30:13 +0100

    Issue #77 : Create Manchester decoding circuit - Added manchester_decoder.vhd file - Added test files
    - Added sdc file

---
+++ assignments/77/manchester_decoder.vhd
@@ -0,0 +1,112 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2023
+-- https://github.com/etf-unibl/pds-2023/
+-----------------------------------------------------------------------------
+--
+-- unit name:     manchester_decoder
+--
+-- description:
+--
+--   This file implements logic that decodes manchester code at it's output.
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2023 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2023 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
+
+-------------------------------------------------------
+--! @manchester_decoder.vhd
+--! @brief Decoder of manchester code
+-------------------------------------------------------
+
+--! Use standard library
+library ieee;
+--! Use logic elements
+use ieee.std_logic_1164.all;
+
+--! Ports of manchester_decoder entity:
+--! clk_i - input clock signal
+--! data_i - input port for coded data
+--! data_o - output port for decoded data
+--! valid_o - port that displays 0 if there is no coded data at the input
+entity manchester_decoder is
+  port (
+  clk_i   : in  std_logic;
+  data_i  : in  std_logic;
+  data_o  : out std_logic;
+  valid_o : out std_logic
+);
+end manchester_decoder;
+
+--! @brief Architecture definition of the Manchester decoder
+--! @details Architecture consists of 3 processes: 
+--! One for the sequential part, i.e. the current state updates
+--! when there is a rising edge of the clock signal;
+--! One for the next state logic, where the input is the data_i pin,
+--! and the output goes into the state register;
+--! The last process is for the state register;
+architecture arch of manchester_decoder is
+  type t_state is
+    (idle, one, zero);
+  signal state_reg, next_reg : t_state;
+  signal prev : std_logic := '0';
+begin
+  -- sequential part
+  process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      state_reg <= next_reg;
+      prev <= data_i;
+    end if;
+  end process;
+  -- next state logic
+  process(data_i, clk_i)
+  begin
+    if falling_edge(clk_i) then
+      if data_i = '1' and prev = '0' then
+        next_reg <= one;
+      elsif data_i = '0' and prev = '1' then
+        next_reg <= zero;
+      else
+        next_reg <= idle;
+      end if;
+    end if;
+  end process;
+  -- output logic
+  process(state_reg)
+  begin
+    data_o <= '0';
+    valid_o <= '0';
+    case state_reg is
+      when idle =>
+      when one =>
+        data_o <= '1';
+        valid_o <= '1';
+      when zero =>
+        data_o <= '0';
+        valid_o <= '1';
+    end case;
+  end process;
+end arch;
---
+++ assignments/77/test/manchester_decoder_tb.vhd
@@ -0,0 +1,146 @@
+-- Copyright (C) 2020  Intel Corporation. All rights reserved.
+-- Your use of Intel Corporation's design tools, logic functions 
+-- and other software and tools, and any partner logic 
+-- functions, and any output files from any of the foregoing 
+-- (including device programming or simulation files), and any 
+-- associated documentation or information are expressly subject 
+-- to the terms and conditions of the Intel Program License 
+-- Subscription Agreement, the Intel Quartus Prime License Agreement,
+-- the Intel FPGA IP License Agreement, or other applicable license
+-- agreement, including, without limitation, that your use is for
+-- the sole purpose of programming logic devices manufactured by
+-- Intel and sold by Intel or its authorized distributors.  Please
+-- refer to the applicable agreement for further details, at
+-- https://fpgasoftware.intel.com/eula.
+
+-- ***************************************************************************
+-- This file contains a Vhdl test bench template that is freely editable to   
+-- suit user's needs .Comments are provided in each section to help the user  
+-- fill out necessary details.                                                
+-- ***************************************************************************
+-- Generated on "11/28/2023 12:42:35"
+                                                            
+-- Vhdl Test Bench template for design  :  manchester_decoder
+-- 
+-- Simulation tool : ModelSim-Altera (VHDL)
+-- 
+
+LIBRARY ieee;                                               
+USE ieee.std_logic_1164.all;                                
+
+ENTITY manchester_decoder_vhd_tst IS
+END manchester_decoder_vhd_tst;
+ARCHITECTURE manchester_decoder_arch OF manchester_decoder_vhd_tst IS
+-- constants                                                 
+-- signals                                                   
+SIGNAL clk_i : STD_LOGIC;
+SIGNAL data_i : STD_LOGIC;
+SIGNAL data_o : STD_LOGIC;
+SIGNAL valid_o : STD_LOGIC;
+COMPONENT manchester_decoder
+	PORT (
+	clk_i : IN STD_LOGIC;
+	data_i : IN STD_LOGIC;
+	data_o : OUT STD_LOGIC;
+	valid_o : OUT STD_LOGIC
+	);
+END COMPONENT;
+BEGIN
+	i1 : manchester_decoder
+	PORT MAP (
+-- list connections between master ports and signals
+	clk_i => clk_i,
+	data_i => data_i,
+	data_o => data_o,
+	valid_o => valid_o
+	);
+                                        
+always : PROCESS                                              
+-- optional sensitivity list                                  
+-- (        )                                                 
+-- variable declarations                                      
+BEGIN                                                         
+        -- code executes for every event on sensitivity list  
+  clk_i <= '0';
+  data_i <= '0';
+  wait for 5 ns;
+  clk_i <= '1';
+  wait for 5 ns;
+  clk_i <= '0';
+  wait for 5 ns;
+  assert(data_o = '0' and valid_o = '0') report "Error: idle state; Expected: data_o = 0, valid_o = 0; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: idle state.";
+  data_i <= '0';
+  clk_i <= '1';
+  wait for 5 ns;
+  data_i <= '1';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '0';
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '1' and valid_o = '1') report "Error: transition from idle state to state one; Expected: data_o = 1, valid_o = 1; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from idle state to state one.";
+  wait for 4 ns;
+  data_i <= '1';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '1';
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '1' and valid_o = '1') report "Error: transition from state one to state one; Expected: data_o = 1, valid_o = 1; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from state one to state one.";
+  wait for 4 ns;
+  data_i <= '0';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '1';
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '0' and valid_o = '1') report "Error: transition from state one to state zero; Expected: data_o = 0, valid_o = 1; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from state one to state zero.";
+  wait for 4 ns;
+  data_i <= '0';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '0';
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '0' and valid_o = '1') report "Error: transition from state zero to state zero; Expected: data_o = 0, valid_o = 1; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from state zero to state zero.";
+  wait for 4 ns;
+  data_i <= '1';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '0';
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '1' and valid_o = '1') report "Error: transition from state zero to state one; Expected: data_o = 1, valid_o = 1; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from state zero to state one.";
+  wait for 4 ns;
+  data_i <= '0';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '1';
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '0' and valid_o = '0') report "Error: transition from state one to idle state; Expected: data_o = 0, valid_o = 0; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from state one to idle state.";
+  wait for 4 ns;
+  data_i <= '0';
+  clk_i <= '0';
+  wait for 5 ns;
+  data_i <= '0';
+  clk_i <= '1';
+  wait for 5 ns;
+  data_i <= '0';
+  clk_i <= '0';
+  wait for 5 ns;
+  clk_i <= '1';
+  wait for 1 ns;
+  assert(data_o = '0' and valid_o = '0') report "Error: transition from state zero to idle state; Expected: data_o = 0, valid_o = 0; Actual: data_o: " & std_logic'image(data_o) & ", valid_o : " & std_logic'image(valid_o) severity failure;
+  report "Assertion successful: transition from state zero to idle state.";
+  wait for 5 ns;
+WAIT;                                                        
+END PROCESS always;                                          
+END manchester_decoder_arch;
