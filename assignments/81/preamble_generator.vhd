1d154509aecf89924f68ffce30e653dde6678e08
Dajana Legenovic <dajana.legenovic@student.etf.unibl.org>
2023-12-17 20:09:33 +0100

    Issue #81 : Preamble generator

--- assignments/81/preamble_generator.vhd
+++ assignments/81/preamble_generator.vhd
@@ -1,10 +1,4 @@
- -----------------------------------------------------------------------------
--- Faculty of Electrical Engineering
--- PDS 2023
--- https://github.com/etf-unibl/pds-2023/
 -----------------------------------------------------------------------------
---
--- unit name:     -----------------------------------------------------------------------------
 -- Faculty of Electrical Engineering
 -- PDS 2023
 -- https://github.com/etf-unibl/pds-2023/
@@ -14,7 +8,7 @@
 --
 -- description:
 --
---   This file implements a simple PREAMBLE GENERATOR logic.
+--   This file implements a simple PREAMBLE logic.
 --
 -----------------------------------------------------------------------------
 -- Copyright (c) 2023 Faculty of Electrical Engineering
@@ -41,37 +35,7 @@
 -- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 -- OTHER DEALINGS IN THE SOFTWARE
 -----------------------------------------------------------------------------
---
--- description:
---
---   This file implements a simple PREAMBLE GENERATOR logic.
---
------------------------------------------------------------------------------
--- Copyright (c) 2023 Faculty of Electrical Engineering
------------------------------------------------------------------------------
--- The MIT License
------------------------------------------------------------------------------
--- Copyright 2023 Faculty of Electrical Engineering
---
--- Permission is hereby granted, free of charge, to any person obtaining a
--- copy of this software and associated documentation files (the "Software"),
--- to deal in the Software without restriction, including without limitation
--- the rights to use, copy, modify, merge, publish, distribute, sublicense,
--- and/or sell copies of the Software, and to permit persons to whom
--- the Software is furnished to do so, subject to the following conditions:
---
--- The above copyright notice and this permission notice shall be included in
--- all copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
--- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
--- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
--- OTHER DEALINGS IN THE SOFTWARE
------------------------------------------------------------------------------
-library ieee; 
+library ieee;
 use ieee.std_logic_1164.all;
 
 entity preamble_generator is
@@ -84,8 +48,8 @@ entity preamble_generator is
 end preamble_generator;
 
 architecture arch of preamble_generator is
- type state_type is (idle, zero, one);
-  signal state_reg, state_next : state_type;
+  type t_state_type is (idle, zero, one);
+  signal state_reg, state_next : t_state_type;
   signal data_next, data_buff_reg : std_logic;
 begin
 
