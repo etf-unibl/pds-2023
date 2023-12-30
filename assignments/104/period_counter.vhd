8f202f06e8cd47b1ba8e865fa5afe0b6c7a5ee6a
Bozana Savkovic <bozana.savkovic@student.etf.unibl.org>
2023-12-30 14:28:27 +0100

    Issue #104 : Create period counter

--- assignments/104/period_counter.vhd
+++ assignments/104/period_counter.vhd
@@ -1,7 +1,45 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2023
+-- https://github.com/etf-unibl/pds-2023/
+-----------------------------------------------------------------------------
+--
+-- unit name:     period_counter
+--
+-- description:
+--
+--   This file implements a simple period_counter.
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2023 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2023 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
+ --! brief Entity for the period counter.
+ --!details This entity represents a simple period counter that counts clock cycles.
 entity period_counter is
     port (
         clk_i    : in  std_logic;
@@ -10,8 +48,11 @@ entity period_counter is
         period_o : out std_logic_vector(9 downto 0)
     );
 end period_counter;
-
+ --! brief Architecture for the period counter.
+ --! This architecture implements a period counter that counts clock cycles between rising edges
+ --! of the period input signal. It outputs the count in the 'period_o' signal.
 architecture arch of period_counter is
+    --! Counters for current and previous values.
     signal current_count, prev_count : unsigned(9 downto 0) := (others => '0');
 begin
     process (clk_i, rst_i)
